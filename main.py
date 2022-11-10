995a6e9885df65f73febc784625e2f81d8c5492e
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2022-11-10 22:23:30 +0900

    add 3d plot

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-size = 25
+plot_3d = False
+size = 20
 state_limit = 3
 neighbor = [
 	(+1, 0),
@@ -12,7 +13,10 @@ neighbor = [
 ]
 
 plt.figure()
-field = 0 * np.random.randint(0, state_limit+1, (size, size))
+if plot_3d:
+	ax = plt.axes(projection='3d')
+
+field = np.random.randint(0, state_limit+1, (size, size))
 while True:
 
 	i, j = np.random.randint(0, size, 2)
@@ -32,11 +36,17 @@ while True:
 			j_ = j_us[index] + ij_[1]
 
 			if 0 <= i_ < size and 0 <= j_ < size:
-				field[i_, j_] += 1
+				field[i_, j_] += (state_limit + 1) / 4
 
 		i_us, j_us = np.where(field > state_limit)
 
 		plt.cla()
-		plt.imshow(field, interpolation="nearest", cmap="gray")
-		plt.clim(0, state_limit+1)
+		if plot_3d:
+			X, Y = np.meshgrid(np.arange(size), np.arange(size))
+			ax.plot_surface(X, Y, field, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+			ax.set_zlim([0, state_limit+1])
+			ax.contour(X, Y, field, colors="black", offset=-1)
+		else:
+			plt.imshow(field, interpolation="nearest", cmap="gray")
+			plt.clim(0, state_limit+1)
 		plt.pause(0.00001)
